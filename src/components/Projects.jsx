51b385b991e0ca4fa023f77fc74a203a1fe9426a
danylo <kutsakd06@gmail.com>
2025-07-23 22:48:57 +0200

    New form

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,18 +14,24 @@ export const Projects = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
               <h3 className="text-xl font-bold mb-2">Bankist App</h3>
-              <p className="text-gray-400 mb-4">Describing the project</p>
+              <p className="text-gray-400 mb-4">
+                A responsive banking app demonstrating frontend-focused
+                full-stack skills, including dynamic UI, secure login, and
+                data-driven features.
+              </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Node.js", "AWS"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                {["HTML", "REST API", "CSS", "DOM", "Algorithms"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
                                     hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
                     "
-                  >
-                    {tech}
-                  </span>
-                ))}
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
               </div>
               <div className="flex justify between items-center">
                 <a
@@ -120,7 +126,7 @@ export const Projects = () => {
               </div>
               <div className="flex justify between items-center">
                 <a
-                  href="https://github.com/"
+                  href="https://github.com/DaielCode/Pig-Game"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-blue-4-- hover:text-blue-300 trasition-colors"
@@ -237,7 +243,7 @@ export const Projects = () => {
                 manipulation, event handling, user authentication, and dynamic
                 UI updates using HTML, CSS, and JavaScript.
               </p>
-              <div>
+              <div className="flex flex-wrap gap-2">
                 {[
                   "HTML",
                   "CSS",
