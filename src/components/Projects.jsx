0709c3a22f41ebc5d409e81e9fd7cb2fda9dcb70
danylo <kutsakd06@gmail.com>
2025-07-23 21:29:39 +0200

    New form

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -50,6 +50,43 @@ export const Projects = () => {
               </div>
             </div>
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+              <h3 className="text-xl font-bold mb-2">Bankist App</h3>
+              <p className="text-gray-400 mb-4">Describing the project</p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "Node.js", "AWS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify between items-center">
+                <a
+                  href="https://github.com/DaielCode/BankApp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-4-- hover:text-blue-300 trasition-colors"
+                >
+                  View Project →{" "}
+                </a>
+              </div>
+              <div>
+                {" "}
+                <a
+                  href="https://daielcode.github.io/BankApp/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-4-- hover:text-blue-300 trasition-colors"
+                >
+                  View Project in Browser →{" "}
+                </a>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
               <h3 className="text-xl font-bold mb-2">Pig Game</h3>
               <p className="text-gray-400 mb-4">
                 Classic two-player Pig Dice game using HTML, CSS, and JavaScript
@@ -163,48 +200,8 @@ export const Projects = () => {
                   View Project →{" "}
                 </a>
               </div>
-              <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-              <h3 className="text-xl font-bold mb-2">Bankist DOM</h3>
-              <p className="text-gray-400 mb-4">
-                Bankist-DOM is a banking app demonstrating advanced DOM
-                manipulation, event handling, user authentication, and dynamic
-                UI updates using HTML, CSS, and JavaScript.
-              </p>
-              <div>
-                {[
-                  "HTML",
-                  "CSS",
-                  "JavaScript",
-                  "DOM",
-                  "Event Handling",
-                  "Data Validation",
-                  "Responsive Design",
-                ].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
-                    "
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-              <div className="flex justify between items-center">
-                <a
-                  href="https://github.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-blue-4-- hover:text-blue-300 trasition-colors"
-                >
-                  View Project →{" "}
-                </a>
-              </div>
-              
             </div>
-            
           </div>
-
         </div>
       </RevealOnScroll>
     </section>
