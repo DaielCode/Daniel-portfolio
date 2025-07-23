8385b91a0c92c860ef2ac0182be63a996f4fc47a
danylo <kutsakd06@gmail.com>
2025-07-23 21:51:57 +0200

    New form

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -86,6 +86,53 @@ export const Projects = () => {
                 </a>
               </div>
             </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+              <h3 className="text-xl font-bold mb-2">Pig Game</h3>
+              <p className="text-gray-400 mb-4">
+                Classic two-player Pig Dice game using HTML, CSS, and JavaScript
+                with turn-based logic and dynamic score updates.
+              </p>
+              <div>
+                {[
+                  "HTML",
+                  "CSS",
+                  "JavaScript",
+                  "DOM",
+                  "Algorithms",
+                  "UI Updates",
+                ].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition whitespace-nowrap
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify between items-center">
+                <a
+                  href="https://github.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-4-- hover:text-blue-300 trasition-colors"
+                >
+                  View Project →{" "}
+                </a>
+              </div>
+              <div>
+                {" "}
+                <a
+                  href="https://daielcode.github.io/Pig-Game/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-4-- hover:text-blue-300 trasition-colors"
+                >
+                  View Project in Browser →{" "}
+                </a>
+              </div>
+            </div>
 
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
               <h3 className="text-xl font-bold mb-2">Pig Game</h3>
@@ -121,17 +168,17 @@ export const Projects = () => {
                 >
                   View Project →{" "}
                 </a>
-                <div>
-                  {" "}
-                  <a
-                    href="https://daielcode.github.io/Pig-Game/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-4-- hover:text-blue-300 trasition-colors"
-                  >
-                    View Project in Browser →{" "}
-                  </a>
-                </div>
+              </div>
+              <div>
+                {" "}
+                <a
+                  href="https://daielcode.github.io/Pig-Game/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-4-- hover:text-blue-300 trasition-colors"
+                >
+                  View Project in Browser →{" "}
+                </a>
               </div>
             </div>
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
